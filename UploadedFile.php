165a951cf49150160d6829bfc3d4bb47fce54c94
Chandler McWilliams <chandler@brysonian.com>
2012-12-10 22:47:43 -0800

    trying to nail animated gif detection

--- UploadedFile.php
+++ UploadedFile.php
@@ -159,9 +159,10 @@ class UploadedFile
     // at least 2 frame headers
     if(!($fh = @fopen($this->get_path(), 'rb'))) return false;
     $count = 0;
-    while(!feof($fh) && $count < 2)
-        $chunk = fread($fh, 1024 * 100); //read 100kb at a time
-        $count += preg_match_all('#\x00\x21\xF9\x04.{4}\x00\x2C#s', $chunk, $matches);
+    while(!feof($fh) && $count < 2) {
+    	$chunk = fread($fh, 1024 * 100); //read 100kb at a time
+      $count += preg_match_all('#\x00\x21\xF9\x04.{4}\x00\x2C#s', $chunk, $matches);
+		}
     fclose($fh);
     return $count > 1;
 	}
